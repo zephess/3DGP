5c3fbd398cc3b55193b7c698a7d6ff5c66b58708
zephess <cmfx7@icloud.com>
2025-06-05 18:11:13 +0100

    removed unnecessary comments in main

--- Proj.cpp
+++ Proj.cpp
@@ -15,24 +15,17 @@
 #define WINDOW_HEIGHT 600
 #undef main
 int main() {
-	/*SDL_Window* window = SDL_CreateWindow("Triangle",
-		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-		WINDOW_WIDTH, WINDOW_HEIGHT,
-		SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
-	if (!SDL_GL_CreateContext(window)) {
-		throw std::runtime_error("Failed to create OpenGL context");
-	}*/
+
 	Window* window = new Window();
 	if (glewInit() != GLEW_OK) {
 		throw std::runtime_error("Failed to initialise GLEW");
 	}
-	//int w = 0;
-	//int h = 0;
+
 	stbi_set_flip_vertically_on_load(true);
 	Model mdl("bird.obj");
 	Model mdl2("bird.obj");
-	//unsigned char* data = stbi_load("Image.png", &w, &h, NULL, 4);
-	unsigned char whitePixel[] = { 255, 255, 0, 255 }; // RGBA
+
+	unsigned char whitePixel[] = { 255, 255, 0, 255 }; 
 	int w = 1, h = 1;
 	unsigned char* data = whitePixel;
 	if (!data) {
@@ -40,14 +33,14 @@ int main() {
 		return 1;
 	}
 	const GLfloat positions[] = {
-		//  X     Y      Z
-		-0.5f,  0.5f, 0.0f,  // Top-left
-		-0.5f, -0.5f, 0.0f,  // Bottom-left
-		 0.5f, -0.5f, 0.0f,  // Bottom-right
+	
+		-0.5f,  0.5f, 0.0f,  
+		-0.5f, -0.5f, 0.0f,  
+		 0.5f, -0.5f, 0.0f, 
 
-		-0.5f,  0.5f, 0.0f,  // Top-left
-		 0.5f, -0.5f, 0.0f,  // Bottom-right
-		 0.5f,  0.5f, 0.0f   // Top-right
+		-0.5f,  0.5f, 0.0f,  
+		 0.5f, -0.5f, 0.0f, 
+		 0.5f,  0.5f, 0.0f   
 	};
 
 	const GLfloat colors[] = {
@@ -57,13 +50,13 @@ int main() {
 	};
 
 	const GLfloat texCoords[] = {
-	0.0f, 1.0f,  // Top-left
-	0.0f, 0.0f,  // Bottom-left
-	1.0f, 0.0f,  // Bottom-right
+	0.0f, 1.0f,  
+	0.0f, 0.0f, 
+	1.0f, 0.0f,  
 
-	0.0f, 1.0f,  // Top-left
-	1.0f, 0.0f,  // Bottom-right
-	1.0f, 1.0f   // Top-right
+	0.0f, 1.0f,  
+	1.0f, 0.0f,  
+	1.0f, 1.0f   
 	};
 
 	GLuint textureId = 0;
@@ -136,93 +129,14 @@ int main() {
 	shader.use();
 	shader.setInt("inTexture", 1);
 
-	//const GLchar* vertexShaderSrc =
-	//
-	//	"uniform mat4 uProjection;"
-	//	"uniform mat4 uModel;"
-	//	"attribute vec3 aPosition;"
-	//	"attribute vec2 aTexCoord;"
-	//	"varying vec2 vTexCoord;"
-	//	"void main(){"
-	//	"gl_Position = uProjection * uModel * vec4(aPosition, 1.0);"
-	//	"vTexCoord = aTexCoord;"
-	//	"}";
 
-	//GLuint vertexShaderId = glCreateShader(GL_VERTEX_SHADER);
-	//glShaderSource(vertexShaderId, 1, &vertexShaderSrc, NULL);
-	//glCompileShader(vertexShaderId);
-	//GLint success = 0;
-	//glGetShaderiv(vertexShaderId, GL_COMPILE_STATUS, &success);
-	//if (!success) {
-	//	throw std::exception();
-	//}
-
-
-	//const GLchar* fragmentShaderSrc =
-	//	"uniform sampler2D inTexture;"
-	//	"varying vec2 vTexCoord;"
-	//	"void main(){"
-	//	"vec4 tex = texture2D(inTexture, vTexCoord);"
-	//	"gl_FragColor = tex;"
-	//	//"gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);"
-	//	"}";
-
-
-	//GLuint fragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
-
-	//glShaderSource(fragmentShaderId, 1, &fragmentShaderSrc, NULL);
-	//glCompileShader(fragmentShaderId);
-	//glGetShaderiv(fragmentShaderId, GL_COMPILE_STATUS, &success);
-
-	//if (!success) {
-	//	throw std::exception();
-	//}
-
-
-	//GLuint programId = glCreateProgram();
-	//glAttachShader(programId, vertexShaderId);
-	//glAttachShader(programId, fragmentShaderId);
-
-	//glBindAttribLocation(programId, 0, "aPosition");
-	//glBindAttribLocation(programId, 1, "aColor");
-	//glBindAttribLocation(programId, 2, "aTexCoord");
-
-	//glLinkProgram(programId);
-	//glGetProgramiv(programId, GL_LINK_STATUS, &success);
-	//if (!success) {
-	//	throw std::exception();
-	//}
-	//GLuint uniformId = glGetUniformLocation(programId, "inTexture");
-	//if (uniformId == -1) {
-	//	throw std::exception();
-	//}
-	//glUseProgram(programId);
 	glActiveTexture(GL_TEXTURE0 + 1);
 	glBindTexture(GL_TEXTURE_2D, textureId);
-	//glBindVertexArray(mdl.vao_id());
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	//glUniform1i(uniformId, 1);
-	//glUseProgram(0);
-	//GLint colorUniformId = glGetUniformLocation(programId, "uColor");
-	/*if (colorUniformId == -1) {
-		throw std::exception();
-	}*/
-
-	//GLint modelLoc = glGetUniformLocation(programId, "uModel");
-	//GLint projectionLoc = glGetUniformLocation(programId, "uProjection");
-
-	/*glDetachShader(programId, vertexShaderId);
-	glDeleteShader(vertexShaderId);
-	glDetachShader(programId, fragmentShaderId);
-	glDeleteShader(fragmentShaderId);*/
+	
 	float angle = 0.0f;
 	glEnable(GL_CULL_FACE);
 	glEnable(GL_DEPTH_TEST);
-	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	//glFrontFace(GL_CW);
+
 	bool quit = false;
 	while (!quit) {
 		SDL_Event ev = { 0 };
@@ -240,12 +154,9 @@ int main() {
 		glViewport(0, 0, width, height);
 
 		glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
-		//glClear(GL_COLOR_BUFFER_BIT);
+		
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		//glUseProgram(programId);
-		//glUniform4f(colorUniformId, 0, 1, 0, 1);
-		//glBindVertexArray(vaoId);
-
+	
 		glBindVertexArray(mdl.vao_id());
 		glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, 0.1f, 100.0f);
 		
@@ -256,8 +167,7 @@ int main() {
 		angle += 1.0f;
 		shader.setMat4("uModel", model);
 		shader.setMat4("uProjection", projection);
-		/*glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));*/
-		//glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+	
 		glDrawArrays(GL_TRIANGLES, 0, mdl.vertex_count());
 		glBindVertexArray(mdl2.vao_id());
 		glm::mat4 model2(1.0f);
@@ -266,20 +176,11 @@ int main() {
 
 		angle += 1.0f;
 		shader.setMat4("uModel", model2);
-		//shader.setMat4("uProjection", projection);
-		/*glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));*/
-		//glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+	
+	
 		glDrawArrays(GL_TRIANGLES, 0, mdl2.vertex_count());
 
-		//glBindVertexArray(mdl2.vao_id());
-		//glm::mat4 model2(1.0f);
-		//shader.setMat4("uModel", model2);
-		//shader.setMat4("uProjection", projection);
-		//model2 = glm::translate(model2, glm::vec3(10, -10, -100.0f));  // Positioned differently
-		//model2 = glm::rotate(model2, glm::radians(angle), glm::vec3(0, 1, 0));
-
-		//glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model2));
-		//glDrawArrays(GL_TRIANGLES, 0, 6);
+		
 		
 		glDrawArrays(GL_TRIANGLES, 0, mdl2.vertex_count());
 		glBindVertexArray(0);
